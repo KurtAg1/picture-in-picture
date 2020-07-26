2baa97021357813f7d437d154657a739b5a3c094
KurtAg1 <kurtagius613@gamil.com>
2020-07-26 15:22:18 +0200

    adding files

---
+++ script.js
@@ -0,0 +1,32 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+    console.log("whoops, error here: ", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  try {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// On Load
+selectMediaStream();
